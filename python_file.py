e95bbcee56af19e0004dac8675e1821de9f94fb8
Sunidhi Singh <76534431+sunidhi09@users.noreply.github.com>
2024-07-09 23:52:39 +0530

    Add files via upload

---
+++ python_file.py
@@ -0,0 +1,17 @@
+# Import the necessary modules
+import requests
+
+# Input the city name
+city = input('Enter City name: ')
+
+# Display the message
+print('Displaying Weather report for: ' + city)
+
+# Fetch the weater details
+url = 'https://wttr.in/{}'.format(city) # Use of format to pass city as a parameter here
+res = requests.get(url)                 # Make use of the requests module
+
+# Display the result
+# Resultant data is stored in res. 
+# Use of the text method to extract our desired weather details to display the result
+print(res.text)
